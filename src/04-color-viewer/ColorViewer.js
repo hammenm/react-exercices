f2580fec10c893bc1ffbaab01a9b942d86c764f2
Maxence Hammen <maxence.hammen@gmail.com>
2022-05-11 11:08:40 +0200

    Upload ColorViewer

--- src/04-color-viewer/ColorViewer.js
+++ src/04-color-viewer/ColorViewer.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './ColorViewer.module.css';
 
 function ColorViewer() {
+  const [text, setText] = useState('');
+  const [colors, setColors] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColors([...colors, text]);
+    setText('');
+  };
+
   return (
     <>
       <h2>Color viewer</h2>
-      <form>
-        <input />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <button type="submit">Submit</button>
       </form>
-      <div className={classes.box} />
+      {colors.map((color, idx) => (
+        <div key={idx} className={classes.box} style={{ backgroundColor: color }} />
+      ))}
     </>
   );
 }
